90be7540e8c06520ff03c687bd14f54ce04a13f1
mmedinafv <altairhopeth@gmail.com>
2024-06-27 20:24:06 -0600

    Commit de archivos-game

---
+++ hangman/static/js/function.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+const qwertyLayout = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+];
+
+const ContenedorLetras = document.querySelector('.teclado');
+
+function CrearTeclado() {
+    qwertyLayout.forEach(fila => {
+        const divFila = document.createElement('div');
+        divFila.className = 'fila';
+
+        fila.forEach(letra => {
+            const buttonIn = document.createElement('button');
+            buttonIn.textContent = letra;
+            divFila.appendChild(buttonIn);
+            buttonIn.addEventListener("click", e => initGame(e.target, letra));
+        });
+
+        ContenedorLetras.appendChild(divFila);
+    });
+}
+
+CrearTeclado();
+});
